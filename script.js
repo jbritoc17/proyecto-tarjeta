7796c21de9e5982055844cca08801eac3c71d9a8
Jean <jbritoc17@outlook.es>
2025-04-23 12:11:02 -0500

    organizando

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 // Seleccionamos el contenedor de la tarjeta
 document.addEventListener("DOMContentLoaded", () => {
-    const target = document.getElementById("perfil-card-container");
-  
-    const observer = new IntersectionObserver(
-      entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add("fade-in");
-            observer.unobserve(entry.target); // Detener observación luego de aplicar la animación
-          }
-        });
-      },
-      {
-        threshold: 0.3, // Se activa cuando el 30% del contenedor está visible
-      }
-    );
-  
-    if (target) {
-      observer.observe(target);
+  const target = document.getElementById("perfil-card-container");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("fade-in");
+          observer.unobserve(entry.target); // Detener observación luego de aplicar la animación
+        }
+      });
+    },
+    {
+      threshold: 0.3, // Se activa cuando el 30% del contenedor está visible
     }
-  });
-  
+  );
+
+  if (target) {
+    observer.observe(target);
+  }
+});
